a33486b7a99f2c6500f74ad8e85bc0948d79167b
mashizora <30516315+mashizora@users.noreply.github.com>
2025-11-25 21:41:08 +0800

    update install-p4

--- ubuntu/scripts/install-p4.sh
+++ ubuntu/scripts/install-p4.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+ARCH=$(arch)
+if [[ "$ARCH" != "x86_64" && "$ARCH" != "aarch64" ]]; then
+    echo "Error: Unsupported architecture." >&2
+    exit 1
+fi
+
 RELNOTES_URL="https://help.perforce.com/helix-core/release-notes/current/relnotes.txt"
 VERSION=$(curl -s "$RELNOTES_URL" | grep -oP -m1 "(?<=Version )\d{4}\.\d")
-DOWNLOAD_URL="https://filehost.perforce.com/perforce/r${VERSION:2}/bin.linux26$(arch)/p4"
+if [[ -z "$VERSION" ]]; then
+    echo "Error: Failed to fetch current perforce version." >&2
+    exit 1
+fi
 
-curl -s "$DOWNLOAD_URL" -o /usr/local/bin/p4
+curl -s "https://filehost.perforce.com/perforce/r${VERSION:2}/bin.linux26${ARCH}/p4" -o /usr/local/bin/p4
 chmod 755 /usr/local/bin/p4
